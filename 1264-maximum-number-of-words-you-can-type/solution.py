03fbc42356fdd39797ff759040fa6b354d514bb6
Mayank Mishra <mishra.mayank.010206@gmail.com>
2025-09-15 14:19:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (52.80%)

---
+++ 1264-maximum-number-of-words-you-can-type/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canBeTypedWords(self, text, brokenLetters):
+        """
+        :type text: str
+        :type brokenLetters: str
+        :rtype: int
+        """
+        l=text.split()
+        count=0
+        for i in l:
+            for  j in brokenLetters:
+                if j in i:
+                    break
+            else:
+                count+=1
+        return count
+        
