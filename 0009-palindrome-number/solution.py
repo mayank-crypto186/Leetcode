3aaec1750d84162f261e50a044f050ea7cefb9bd
Mayank Mishra <mishra.mayank.010206@gmail.com>
2025-09-15 14:35:04 +0000

    Sync LeetCode submission Runtime - 4 ms (90.59%), Memory - 12.5 MB (17.33%)

---
+++ 0009-palindrome-number/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        temp = x
+        rev = 0
+        while temp > 0:
+            num = temp % 10 
+            rev = rev * 10 + num
+            temp = temp // 10
+        return rev == x
+        
