e1f3c7fdbdf0de20154b1cdb8a4531ff2ff9dc7c
Mayank Mishra <mishra.mayank.010206@gmail.com>
2025-10-09 07:09:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.8 MB (100.00%)

---
+++ 0617-merge-two-binary-trees/solution.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2) {
+    if(!root1 && !root2)return NULL;
+    if(!root2)return root1;
+    if(!root1)return root2;
+    root1->val+=root2->val;
+    root1->left=mergeTrees(root1->left,root2->left);
+    root1->right=mergeTrees(root1->right,root2->right);
+    return root1;
+}
