2c8f3c17c70e539efaea806cb39e66dfd40f9326
Mayank Mishra <mishra.mayank.010206@gmail.com>
2026-02-16 15:13:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 50.2 MB (68.34%)

---
+++ 0485-max-consecutive-ones/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxCount=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+                if(maxCount<count){
+                maxCount=count;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        return maxCount;
+        
+    }
+};
