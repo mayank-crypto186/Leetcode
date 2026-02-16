851eb224c1ee3af661e9eb7aed331d7b284b737f
Mayank Mishra <mishra.mayank.010206@gmail.com>
2026-02-16 15:54:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (21.91%)

---
+++ 1552-build-an-array-with-stack-operations/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ops;
+        int j = 0;
+
+        for (int i = 1; i <= n && j < target.size(); i++) {
+            ops.push_back("Push");
+
+            if (i == target[j]) {
+                j++;
+            } else {
+                ops.push_back("Pop");
+            }
+        }
+        return ops;
+    }
+};
